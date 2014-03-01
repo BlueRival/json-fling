05c698af8ba35af7e2b3a5eec08f067813213d47
Anthony Hildoer <anthony@bluerival.com>
2014-03-01 00:06:28 -0700

    release 0.2.1: fix bug in module interface

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ module.exports.Receiver = require( './lib/receive' );
 module.exports.Sender = require( './lib/send' );
 
 module.exports.transports = {
-  receivers: {
-    Express: require( './lib/transport.receiver.express' )
-  },
-  senders:   {
-    Http: require( './lib/transport.sender.http' )
-  }
+	receivers: {
+		Express: require( './lib/transport.receiver.express' ),
+		Local:   require( './lib/transport.receiver.local' )
+	},
+	senders:   {
+		Http: require( './lib/transport.sender.http' )
+	}
 };
