732f26a6ef205dde5d1757d6dbfbc981517d2f3a
Anthony Hildoer <anthony@bluerival.com>
2014-03-03 22:58:43 -0700

    release 0.3.0: better addError parameters for response class

--- test/rpcModules/errorModule.js
+++ test/rpcModules/errorModule.js
@@ -3,15 +3,15 @@
 module.exports.action = function ( request, response ) {
 
 	response.hasError();
-	response.addError( 404, 'you should see this in error.message', {string: 'you should see this in error.message.data.string' } );
+	response.addError( 404, 'you should see this in error.message', { string: 'you should see this in error.message.data.string' } );
 	response.send();
 
 };
 
 module.exports.actionMultiple = function ( request, response ) {
 
-	response.addError( 404, 'you should see this in error.data[0].message', {string: 'you should see this in error.data[0].message.data.string' } );
-	response.addError( 404, 'you should see this in error.data[1].message', {string: 'you should see this in error.data[1].message.data.string' } );
+	response.addError( 404, 'you should see this in error.data[0].message', { string: 'you should see this in error.data[0].message.data.string' } );
+	response.addError( { code: 404, message: 'you should see this in error.data[1].message', data: { string: 'you should see this in error.data[1].message.data.string' } } );
 
 	response.send();
 
@@ -19,8 +19,8 @@ module.exports.actionMultiple = function ( request, response ) {
 
 module.exports.actionSendPayload = function ( request, response ) {
 
-	response.addError( 404, 'you should see this in error.data[0].message', {string: 'you should see this in error.data[0].message.data.string' } );
-	response.addError( 404, 'you should see this in error.data[1].message', {string: 'you should see this in error.data[1].message.data.string' } );
+	response.addError( { code: 404, message: 'you should see this in error.data[0].message', data: { string: 'you should see this in error.data[0].message.data.string' } } );
+	response.addError( 404, 'you should see this in error.data[1].message', { string: 'you should see this in error.data[1].message.data.string' } );
 
 	response.send( { hello: 'world' } );
 
